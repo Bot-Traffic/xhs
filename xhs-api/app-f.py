42bf593644d27849d2d29a93670a704652c9b212
ChillyForest <1394466835@qq.com>
2023-11-16 11:09:58 +0800

    chore: update

--- xhs-api/app-f.py
+++ xhs-api/app-f.py
@@ -65,4 +65,4 @@ async def hello_world(request: Request):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5005)
+    uvicorn.run("app-f:app", host="0.0.0.0", port=5005)
